5b5d0f38d04f021db17fc4efa201ea6a1b471de8
Chandra8080 <githubchandra@gmail.com>
2025-06-06 12:19:13 +0700

    Create ads.js

---
+++ assets/scripts/ads.js
@@ -0,0 +1,14 @@
+    const adLink = "https://otieu.com/4/8908364";
+    const toolNavLinks = document.querySelectorAll('#nav-menu .nav-link');
+
+    toolNavLinks.forEach(link => {
+        const href = link.getAttribute('href');
+        if (href && href !== '/' && !href.startsWith('#') && !href.startsWith('javascript:')) {
+            link.addEventListener('click', function(event) {
+                event.preventDefault(); 
+                window.open(this.href, '_blank'); 
+                window.location.href = adLink;
+            });
+        }
+    });
+   (function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('gizokraijaw.net',9385013,document.createElement('script'))
